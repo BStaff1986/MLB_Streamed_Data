ce9850ccdac80c996a05902e06342f472b6fd18a
BStaff1986 <BStaff@gmail.com>
2017-05-13 00:24:38 -0400

    Reorganize class variables for easier function access

--- mlb.py
+++ mlb.py
@@ -1,8 +1,15 @@
-# Program to read MLB JSON Data
+#!python3
+
+# mlb.py :  Program to read and display MLB JSON Data
+
+# TODO: BIGGEST ISSUE! - Only last pitch in atbat appears. MLB Json not quick enough?
+#       Or better algo needs to be written?
 
 # TODO: Future issue - Starting program before game and having program
 # 		become unstable from get_game_events()
-
+# TODO: Perhaps move current atbat function into pitch_update
+#		and return it's value to class variables so multiple functions
+# 		have access without needing to call it multiple times
 import requests
 import json
 import subprocess as sp
@@ -12,20 +19,25 @@ from bs4 import BeautifulSoup
 
 class MLB_Scrape():
 	def __init__(self):
-		self.ab_num = 0
+		# Class variables
+		self.inning = 0
+		self.t_or_b = 'top'
+		self.atbat = '' 
 		self.pitch_id = 0
 		self.json_events = ''
 		self.roster = {}
 
+		# Game of interest URL components
 		self.url = 'http://gd2.mlb.com/components/game/mlb/'
 		self.year = '/year_2017'
 		self.month = '/month_05'
-		self.day = '/day_11'
-		self.game = '/gid_2017_05_11_detmlb_anamlb_1' 
+		self.day = '/day_12'
+		self.game = '/gid_2017_05_12_detmlb_anamlb_1' 
 		self.game_url = (self.url + self.year + self.month +
 						self.day + self.game)
-		print(self.game_url)
-
+		# Start up functions
+		self.get_rosters()
+		
 	def get_game_events(self):
 		'''
 		This function gets the game events JSON data from MLB.com	
@@ -67,57 +79,68 @@ class MLB_Scrape():
 		This function parses the XML data finds the current inning
 		'''
 		inning = self.json_events['data']['game']['inning']
-		inning_num = len(inning) - 1
+		self.inning = len(inning) - 1
 		
-		if not inning[inning_num]: 
-			top_or_bottom = 'top'
+		if not inning[self.inning]: 
+			self.t_or_b = 'top'
 		else:
-			top_or_bottom = 'bottom'
-		
-		return (top_or_bottom, inning_num)
+			self.t_or_b = 'bottom'
 
+		
 	def get_current_atbat(self):
 		'''
 		This function gets the data from the last pitch
 		'''
-		t_or_b, inn_num = self.get_inning()
+		t_or_b, inn_num = self.t_or_b, self.inning
 		try:
 			atbat = self.json_events['data']['game']['inning'][inn_num][t_or_b]['atbat']
 		except: # Find the name of the error and make this stronger!
 			return None
 		
-		current_atbat = atbat[len(atbat) -1]
+		self.atbat = atbat[len(atbat) -1]
 		
-		return current_atbat
-	
 	def get_last_pitch(self):
 		'''
 		This function gets the pitch speed and type from the most
 		recent pitch
 		'''
-		current_ab = self.get_current_atbat()
 
-		pitches = current_ab['pitch']
+		pitches = self.atbat['pitch']
 		last_pitch = pitches[len(pitches) -1]
 		
-		pitch_speed = last_pitch['start_speed']
-		pitch_type = last_pitch['pitch_type']
+		sv_id = int(last_pitch['sv_id'].replace('_',""))
+		
+		if sv_id > self.pitch_id:
+			pitch_speed = last_pitch['start_speed']
+			pitch_type = last_pitch['pitch_type']
+			self.pitch_id = sv_id
+			return pitch_speed, pitch_type
+		else:
+			return None, None
 
-		return pitch_speed, pitch_type
+		
 
 	def pitch_update(self):
 		'''
 		This is the executive function that gets refreshed continuously
 		'''
 		self.get_game_events()
-		speed, p_type = self.get_last_pitch()
+		# Get and check inning
+		self.get_inning()
+		# Get and check atbat event num
+		self.get_current_atbat()
+		# Get and check pitch sv_id num
 
-		print('Pitch Speed: {} Pitch Type: {}'.format(speed, p_type))
+		speed, p_type = self.get_last_pitch()
+		if speed == None and p_type == None:
+			print('No pitch thrown in this atbat')
+		else:
+			print('Pitch Speed: {} Pitch Type: {}'.format(speed, p_type))
 
 
 
 sp.call('clear',shell=True)
 mlb = MLB_Scrape()
-mlb.pitch_update()
-
-ros, away, home, dic = mlb.get_rosters()
+while True:
+	mlb.pitch_update()
+	sleep(3)
